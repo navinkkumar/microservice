ffd2233eb706d2fa5b18855ecb48e0e0f29c637e
navinkrishnak <navienkmr@gmail.com>
2017-09-11 14:03:49 +0530

    base

---
+++ src/main/java/com/poc/ml/microservices/App.java
@@ -0,0 +1,21 @@
+package com.poc.ml.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
